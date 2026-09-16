af52819f96ff218476e2742b53d10fff5c572a04
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-16 22:33:56 +0100

    Implement parseval::ThreadPool() constructor

--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -1,12 +1,5 @@
-#pragma once
+#include "parseval/core/thread_pool.hpp"
 
-#include<condition_variable>
-#include<functional>
-#include<future>
-#include<mutex>
-#include<queue>
-#include<thread>
-#include<vector>
 
 // --------------------------------------------------------------------
 // -*- begin::namespace::parseval                                   -*-
@@ -14,23 +7,35 @@
 namespace parseval{
 // -
 
+ThreadPool::ThreadPool(std::size_t thread_count){
+    if(thread_count==0){
+        thread_count = 1;
+    }
+    this->m_workers.reserve(thread_count);
 
+    for(std::size_t i=0; i < thread_count; ++i){
+        this->m_workers.emplace_back([this](){ this->worker_loop();});
+    }
+}
+
+/*
 class ThreadPool{
 public:
-    explicit ThreadPool(std::size_t thread_count=std::thread::hardware_concurrency());
+
 
     ThreadPool(const ThreadPool&) = delete;
     ThreadPool& operator=(const ThreadPool&) = delete;
 
-    ~ThreadPool();
+ThreadPool::~ThreadPool();
 
-    std::size_t size(void) const noexcept;
+std::size_t ThreadPool::size(void) const noexcept;
 
-    template<typename Function, typename... Args>
-    auto submit(
-        Function&& function,
-        Args&&... args
-    ) -> std::future<std::invoke_result_t<Function, Args...>>;
+template<typename Function, typename... Args>
+auto ThreadPool::submit(
+    Function&& function,
+    Args&&... args
+) -> std::future<std::invoke_result_t<Function, Args...>>;
+
 
 private:
     using Task = std::function<void()>;
@@ -42,7 +47,8 @@ private:
     std::condition_variable m_condition;
     bool m_stopping = false;
 
-    void worker_loop(void);
+void ThreadPool::worker_loop(void);
+
 };
 
 static inline ThreadPool& global_thread_pool(void){
@@ -50,7 +56,9 @@ static inline ThreadPool& global_thread_pool(void){
     return pool;
 }
 
+*/
 
 // --------------------------------------------------------------------
 }//-*- end::namespace::parseval                                     -*-
 // --------------------------------------------------------------------
+
