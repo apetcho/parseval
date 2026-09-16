69db80a4350076c0598056dfd9cd77322e5d8097
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-16 21:38:14 +0100

    Implement parseval::Array::operator-=(const Array&) method

--- src/array.cpp
+++ src/array.cpp
@@ -403,6 +403,17 @@ Array& Array::operator+=(const Array& other){
     return *this;
 }
 
+// -
+Array& Array::operator-=(const Array& other){
+    this->validate_same_shape(other);
+
+    for(std::size_t i=0; i < this->size(); ++i){
+        this->m_data[i] -= other.m_data[i];
+    }
+
+    return *this;
+}
+
 /*
 class Array{
 public:
@@ -410,7 +421,7 @@ public:
     using Shape = std::vector<std::size_t>;
 
 
-Array& Array::operator-=(const Array& other){}
+
 Array& Array::operator*=(const Array& other){}
 Array& Array::operator/=(const Array& other){}
 
