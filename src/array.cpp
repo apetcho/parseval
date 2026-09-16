6e85dcb5ab05d7381193641617b35a3c869601a8
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-16 21:30:43 +0100

    Implement parseval::Array::operator/(..) method

--- src/array.cpp
+++ src/array.cpp
@@ -340,7 +340,9 @@ Array Array::operator/(const Array& other) const{
     this->validate_same_shape(other);
     Array result(this->m_shape);
     for(std::size_t i=0; i < this->size(); ++i){
-        if(other.m_data[i] == 0.0){}
+        if(other.m_data[i] == 0.0){
+            throw ParsevalError("Division by zero");
+        }
         result.m_data[i] = this->m_data[i] / other.m_data[i];
     }
 
