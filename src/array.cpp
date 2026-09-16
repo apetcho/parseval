1db3c02bbc3270103ef2faac8a2e5908ea049940
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-16 21:33:04 +0100

    Implement parseval::Array::operator-(..) method

--- src/array.cpp
+++ src/array.cpp
@@ -359,6 +359,15 @@ Array Array::operator+(value_type scalar) const{
     return result;
 }
 
+// -
+Array Array::operator-(value_type scalar) const{
+    Array result(*this);
+    for(auto& value: result.m_data){
+        value -= scalar;
+    }
+
+    return result;
+}
 
 /*
 class Array{
@@ -367,7 +376,7 @@ public:
     using Shape = std::vector<std::size_t>;
 
 
-Array Array::operator-(value_type scalar) const{}
+
 Array Array::operator*(value_type scalar) const{}
 Array Array::operator/(value_type scalar) const{}
 
