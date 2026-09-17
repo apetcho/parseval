38ca1e49728809216ad48cf4571a05f4b974882f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-17 05:05:26 +0100

    Refactor parseval::Array::operator+(Array::value_type scalar) method

--- src/array.cpp
+++ src/array.cpp
@@ -394,12 +394,12 @@ Array Array::operator/(const Array& other) const{
 
 // -
 Array Array::operator+(value_type scalar) const{
-    Array result(*this);
-    for(auto& value: result.m_data){
-        value += scalar;
-    }
-
-    return result;
+    return this->elementwise_scalar(
+        scalar,
+        [](Array::value_type val, Array::value_type scalar_value){
+            return val + scalar_value;
+        }
+    );
 }
 
 // -
